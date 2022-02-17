30400b04509a316dbbc5c52d723de1b79cff4803
04irsyaD <98296563+04irsyaD@users.noreply.github.com>
2022-02-17 22:59:36 +0700

    first commit

---
+++ src/matemaatika1/mtk2.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package matemaatika1;
+
+/**
+ *
+ * @author Irsyad
+ */
+public class mtk2 {
+  private int  a = 9;
+  private int b = 10;
+  private int ps;
+  private int p;
+  private int jgg;
+  private String p1;
+  private String  pp;
+  private String jg;
+  
+  public void rumuspersegi(){
+      p1 = "s*s";
+      System.out.println("Rumus  Luas persegi = "+p1);
+      p = b+b;
+      System.out.println("hasil rumus persegi = "+b);
+      
+  }
+  private void rumuspersegipanjang(){
+      pp ="p*l";
+      System.out.println("Rumus  Luas Persegi panjang = "+pp);
+      ps = a+b;
+      System.out.println("Hasil rumus persegi panjang ="+ps);
+      
+  }
+  protected void rumusjajargenjang(){
+   jg = "a*t";
+      System.out.println("Rumus Luas Jajar Genjang ="+jg);
+   
+   jgg = a+a;
+      System.out.println("Hasil rumus jajar genjang = "+jgg);
+   
+  }
+}
---
+++ src/newpackage3/mtk6.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage3;
+
+import matemaatika1.mtk2;
+
+/**
+ *
+ * @author Irsyad
+ */
+public class mtk6 {
+    public void main(String[]args) {
+     mtk2 a1 = new mtk2();
+        a1.rumuspersegi();
+     
+     
+     
+    }
+  
+ 
+  
+}
+
+
+
